87b5718e2096134fb34ab8a822105650b2fd3164
Guilherme Camargo <f0fp1429@t-systems.com.br>
2022-11-09 11:59:17 -0300

    Added initial security config

---
+++ wms/src/main/java/com/twms/wms/security/configuration/WebSecurityConfig.java
@@ -0,0 +1,20 @@
+package com.twms.wms.security.configuration;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .cors().and()
+                .csrf().disable()
+                .httpBasic().disable();
+    }
+}
