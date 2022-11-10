e1d4ade44596a4960891b9e43aa3f26309b9611d
Rafael Prates <rafaelbprates@gmail.com>
2022-11-10 08:34:17 -0400

    added controller/service/repository for CRUD operations on Address resource (#2)

--- wms/src/main/java/com/twms/wms/entities/Address.java
+++ wms/src/main/java/com/twms/wms/entities/Address.java
@@ -1,11 +1,14 @@
 package com.twms.wms.entities;
 
+import com.twms.wms.dtos.AddressDTO;
 import lombok.Data;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 
 @Entity
@@ -16,13 +19,23 @@ public class Address {
     @GeneratedValue( strategy = GenerationType.IDENTITY)
     private Long id;
     @NotBlank
-    private String zipCode;
-    @NotBlank
-    private String state;
-    @NotBlank
-    private String city;
-    @NotBlank
     private String street;
     @NotBlank
     private String number;
+    @NotBlank
+    private String city;
+    @NotBlank
+    private String state;
+    @NotBlank
+    private String zipCode;
+
+    public Address fromDTO(AddressDTO addressDTO) {
+        Address address = new Address();
+        address.setStreet(addressDTO.getStreet());
+        address.setNumber(addressDTO.getNumber());
+        address.setCity(addressDTO.getCity());
+        address.setState(addressDTO.getState());
+        address.setZipCode(addressDTO.getZipCode());
+        return address;
+    }
 }
