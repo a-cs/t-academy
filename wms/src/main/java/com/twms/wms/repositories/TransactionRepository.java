70c4afd0c3dde8c2f7a7df4a1ee8f38e01513c3a
heavymsv <matheus.santos.vieira@gmail.com>
2022-11-10 16:20:02 -0300

    Finished Entity, repository, services and controllers for Transaction, class Inventory no added, making it difficult to test, for now this step will be delayed.

---
+++ wms/src/main/java/com/twms/wms/repositories/TransactionRepository.java
@@ -0,0 +1,9 @@
+package com.twms.wms.repositories;
+
+import com.twms.wms.entities.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction,Long> {
+}
