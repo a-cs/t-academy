2e7369fedf61128b46ea95c2e0b84e2664f31096
Luiz Cirilo Tomasi Neto <65315838+Luiz-Tomasi@users.noreply.github.com>
2022-11-10 09:35:30 -0300

    Created CRUD for Category (#4)

--- wms/src/main/java/com/twms/wms/repositories/AddressRepository.java
+++ wms/src/main/java/com/twms/wms/repositories/AddressRepository.java
@@ -2,6 +2,8 @@ package com.twms.wms.repositories;
 
 import com.twms.wms.entities.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AddressRepository extends JpaRepository<Address, Long> {
 }
