e49b84d980ff9b9f71893ae195e5b67359bcb2ef
Anderson Carneiro Sousa <63657280+a-cs@users.noreply.github.com>
2022-11-10 09:32:56 -0300

    creating crud for SKU (#1)

---
+++ wms/src/main/java/com/twms/wms/controllers/SKUController.java
@@ -0,0 +1,42 @@
+package com.twms.wms.controllers;
+
+import com.twms.wms.entities.SKU;
+import com.twms.wms.services.SKUService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/sku")
+public class SKUController {
+
+    @Autowired
+    SKUService service;
+
+    @GetMapping
+    public ResponseEntity<List<SKU>> read() {
+        return ResponseEntity.status(HttpStatus.OK).body(service.read());
+    }
+
+    @PostMapping
+    public ResponseEntity<SKU> create(@Valid @RequestBody SKU sku) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.save(sku));
+    }
+
+    @PutMapping("/{skuId}")
+    public ResponseEntity<SKU> update(@PathVariable("skuId") Long skuId, @Valid @RequestBody SKU sku) {
+        return ResponseEntity.status(HttpStatus.OK).body(service.update(skuId, sku));
+    }
+
+    @DeleteMapping("/{skuId}")
+    public ResponseEntity<SKU> update(@PathVariable("skuId") Long skuId) {
+        service.delete(skuId);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+
+}
---
+++ wms/src/main/java/com/twms/wms/repositories/SKURepository.java
@@ -0,0 +1,8 @@
+package com.twms.wms.repositories;
+
+import com.twms.wms.entities.SKU;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SKURepository extends JpaRepository<SKU, Long> {
+
+}
