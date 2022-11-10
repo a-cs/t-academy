aaa3e39c8575ee3538a186a72a5c952a229d53e5
Guilherme Camargo <f0fp1429@t-systems.com.br>
2022-11-10 04:37:12 -0300

    Basic User crud implementation

--- wms/src/main/java/com/twms/wms/repositories/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.twms.wms.repositories;
-
-import com.twms.wms.entities.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-}
---
+++ wms/src/main/java/com/twms/wms/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.twms.wms.repositories;
+
+import com.twms.wms.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findUserByUsername(String username);
+}
