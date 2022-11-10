9e977d6ae6932834ab57ae088165673b2afcc4ae
HeavyMSV <81862091+heavymsv@users.noreply.github.com>
2022-11-10 09:37:49 -0300

    Revert "Cru dclients branch (#5)"
    This reverts commit 6e07e0d24bb94064f17b069ffe5467dd7d5979e2.

--- wms/src/main/java/com/twms/wms/exceptions/NoSuchBranchException.java
+++
@@ -1,7 +0,0 @@
-package com.twms.wms.exceptions;
-
-public class NoSuchBranchException extends RuntimeException{
-    public NoSuchBranchException(String message) {
-        super(message);
-    }
-}
--- wms/src/main/java/com/twms/wms/repositories/RoleRepository.java
+++ wms/src/main/java/com/twms/wms/repositories/RoleRepository.java
@@ -2,8 +2,6 @@ package com.twms.wms.repositories;
 
 import com.twms.wms.entities.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
 }
