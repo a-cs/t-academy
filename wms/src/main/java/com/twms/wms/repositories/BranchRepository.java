6e07e0d24bb94064f17b069ffe5467dd7d5979e2
HeavyMSV <81862091+heavymsv@users.noreply.github.com>
2022-11-10 09:34:49 -0300

    Cru dclients branch (#5)
    * First push with full entity and repository for branch entity
    
    * First push with full branch basic CRUD
    
    * First push with full branch basic CRUD
    
    * First push with full branch basic CRUD

--- wms/src/main/java/com/twms/wms/repositories/BranchRepository.java
+++ wms/src/main/java/com/twms/wms/repositories/BranchRepository.java
@@ -1,9 +1,9 @@
 package com.twms.wms.repositories;
 
-import com.twms.wms.entities.Address;
+import com.twms.wms.entities.Branch;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AddressRepository extends JpaRepository<Address, Long> {
+public interface BranchRepository extends JpaRepository<Branch,Long> {
 }
