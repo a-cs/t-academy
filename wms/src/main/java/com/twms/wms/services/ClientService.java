f428c6cf482a658628e6b909c6fb3d2cda01c86d
heavymsv <matheus.santos.vieira@gmail.com>
2022-11-10 14:09:07 -0300

    Finished Entity, repository and services. created a exception for when a client isn't found. working on endpoints now

---
+++ wms/src/main/java/com/twms/wms/services/ClientService.java
@@ -0,0 +1,46 @@
+package com.twms.wms.services;
+
+import com.twms.wms.entities.Client;
+import com.twms.wms.exceptions.NoSuchClientException;
+import com.twms.wms.repositories.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ClientService {
+
+    @Autowired
+    ClientRepository clientRepository;
+
+    @Transactional
+    public Client createClient(Client client){return clientRepository.save(client);}
+
+    public List<Client> readAllClients(){return clientRepository.findAll();}
+
+    public Client readClientById(Long clientId){
+        Optional<Client> optionalClient = clientRepository.findById(clientId);
+        return optionalClient.orElseThrow(()->new NoSuchClientException("Branch Not Created or Removed!!"));
+    }
+
+    public Client updateClient(Long clientId, Client client){
+        Client oldClient = this.readClientById(clientId);
+
+        oldClient.setAdress(client.getAdress());
+        oldClient.setName(client.getName());
+        oldClient.setCNPJ(client.getCNPJ());
+        oldClient.setUser(client.getUser());
+
+        return this.createClient(oldClient);
+    }
+
+    @Transactional
+    public void deleteClient(Long clientId){
+        Client toDelete = this.readClientById(clientId);
+        clientRepository.delete(toDelete);
+    }
+
+}
