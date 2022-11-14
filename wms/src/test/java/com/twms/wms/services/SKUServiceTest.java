1b78a6179a3c8474f678c70a395c9fc8a70717d9
Anderson Carneiro Sousa <acarneirosousa@gmail.com>
2022-11-14 16:14:10 -0300

    creating SKU services tests

---
+++ wms/src/test/java/com/twms/wms/services/SKUServiceTest.java
@@ -0,0 +1,61 @@
+package com.twms.wms.services;
+
+import com.twms.wms.entities.SKU;
+import com.twms.wms.repositories.SKURepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@ExtendWith(SpringExtension.class)
+public class SKUServiceTest {
+
+    @InjectMocks
+    private SKUService service;
+
+    @Mock
+    private SKURepository repository;
+
+    @Test
+    public void returnListAfterRead() {
+        List<SKU> list = new ArrayList<>();
+
+        Mockito.when(repository.findAll()).thenReturn(list);
+        Assertions.assertNotNull(service.read());
+        Mockito.verify(repository,Mockito.times(1)).findAll();
+    }
+
+    @Test
+    public void returnSKUAfterSave() {
+        SKU sku = new SKU();
+        sku.setId(1L);
+        sku.setName("testName");
+        sku.setDescription("testDescription");
+
+        Mockito.when(repository.save(sku)).thenReturn(sku);
+        Assertions.assertNotNull(service.save(sku));
+        Mockito.verify(repository,Mockito.times(1)).save(sku);
+    }
+
+
+    @Test
+    public void returnNothingAfterDelete() {
+        SKU sku = new SKU();
+        sku.setId(1L);
+        sku.setName("testName");
+        sku.setDescription("testDescription");
+
+        Mockito.when(repository.findById(sku.getId())).thenReturn(Optional.of(sku));
+
+        Mockito.doNothing().when(repository).delete(sku);
+        Assertions.assertDoesNotThrow(() -> service.delete(sku.getId()));
+        Mockito.verify(repository,Mockito.times(1)).delete(sku);
+    }
+}
