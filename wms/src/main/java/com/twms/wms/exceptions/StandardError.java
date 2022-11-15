8a1ee245196f33a61f4a7a6745317a2cf3356f9e
Bianca Eliza Corrêa <bianca.correa@t-systems.com>
2022-11-15 20:53:26 -0300

    Created ResourceExceptionHandler for EntityNotFound exception

---
+++ wms/src/main/java/com/twms/wms/exceptions/StandardError.java
@@ -0,0 +1,64 @@
+package com.twms.wms.exceptions;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+public class StandardError implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private Instant timeStamp;
+    private int status;
+    private String error;
+    private String message;
+    private String path;
+
+    public StandardError() {
+    }
+
+    public StandardError(Instant timeStamp, int status, String error, String message, String path) {
+        this.timeStamp = timeStamp;
+        this.status = status;
+        this.error = error;
+        this.message = message;
+        this.path = path;
+    }
+
+    public Instant getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(Instant timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+}
