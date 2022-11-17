1a958691c0ae3cb831264ad35dc979b630323284
Bianca Eliza Corrêa <bianca.correa@t-systems.com>
2022-11-16 21:09:29 -0300

    Created NoSuchMEasurementUnitException and updated ResourceExceptionHandler

---
+++ wms/src/main/java/com/twms/wms/exceptions/NoSuchMeasurementUnitException.java
@@ -0,0 +1,7 @@
+package com.twms.wms.exceptions;
+
+public class NoSuchMeasurementUnitException extends RuntimeException{
+    public NoSuchMeasurementUnitException(String message) {
+        super(message);
+    }
+}
