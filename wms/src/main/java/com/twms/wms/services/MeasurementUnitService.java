747f763113b455964acc8b5dce05e386bc693e29
Bianca Eliza Corrêa <bianca.correa@t-systems.com>
2022-11-17 14:40:04 -0300

    Throwing IllegalArgumentException in MeasurementUnitService

--- wms/src/main/java/com/twms/wms/services/MeasurementUnitService.java
+++ wms/src/main/java/com/twms/wms/services/MeasurementUnitService.java
@@ -22,6 +22,12 @@ public class MeasurementUnitService {
 
     @Transactional
     public MeasurementUnit create(MeasurementUnit measurementUnit) {
+        if(measurementUnit.getDescription() == null) {
+            throw new IllegalArgumentException("Measurement unit description is missing");
+        }
+        if(measurementUnit.getSymbol() == null) {
+            throw new IllegalArgumentException("Measurement unit symbol is missing");
+        }
         return measurementUnitRepository.save(measurementUnit);
     }
 
