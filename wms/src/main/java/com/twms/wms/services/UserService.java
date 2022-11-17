8798573c8da49488df3b5f7c78a51807e4011a15
Guilherme Camargo <f0fp1429@t-systems.com.br>
2022-11-17 16:57:26 -0300

    User tests implemented

--- wms/src/main/java/com/twms/wms/services/UserService.java
+++ wms/src/main/java/com/twms/wms/services/UserService.java
@@ -7,8 +7,6 @@ import com.twms.wms.enums.AccessLevel;
 import com.twms.wms.repositories.RoleRepository;
 import com.twms.wms.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -17,6 +15,8 @@ import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityNotFoundException;
 import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService implements UserDetailsService {
@@ -35,7 +35,6 @@ public class UserService implements UserDetailsService {
             throw new IllegalStateException("Username already registered.");
         }
         user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
-        //TODO: Consultar role no banco??
         user.addAccessLevel(roleRepository.findByAuthority(AccessLevel.ROLE_CLIENT));
         User savedUser = userRepository.save(user);
         return new UserDTO(savedUser);
@@ -46,37 +45,31 @@ public class UserService implements UserDetailsService {
         clientUser.setUsername(username);
         //TODO: Auto generate random password
         String clientPassword = username;
-        clientUser.setPassword(username);
+        clientUser.setPassword(clientPassword);
         User savedClientUser = new User(this.createUser(clientUser));
         savedClientUser.setPassword(clientPassword);
         return savedClientUser;
     }
 
     public UserDTO getUserByUsername(String username){
-
-        User user = userRepository.findUserByUsername(username).orElseThrow(
-                ()->new UsernameNotFoundException("User not found.")
-        );
+        User user = (User) this.loadUserByUsername(username);
         return new UserDTO(user);
     }
 
-    public UserDTO userLogin(User user){
-        User userRegisted = userRepository.findUserByUsername(user.getUsername()).orElseThrow(
-                ()->new UsernameNotFoundException("User not found.")
-        );
-        boolean isAuthenticated = bCryptPasswordEncoder.matches(user.getPassword(), userRegisted.getPassword());
-
-        if(isAuthenticated){
-            return new UserDTO(userRegisted);
-        }
-        throw new BadCredentialsException("Wrong user or password.");
+    public List<UserDTO> getAllUsers(){
+        List<User> userList = userRepository.findAll();
+        List<UserDTO> userDTOList = userList.stream().map(user->new UserDTO(user)).collect(Collectors.toList());
+        return userDTOList;
     }
 
-    public UserDTO updateUserAccessLevel(User user, Long userId){
+    public UserDTO updateUserAccessLevel(Role role, Long userId){
         User savedUser = userRepository.findById(userId).orElseThrow(
                 ()->new EntityNotFoundException("User not found.")
         );
-        savedUser.setAccessLevel(user.getAccessLevel());
+        Role newRole = roleRepository.findById(role.getId()).orElseThrow(
+                ()->new EntityNotFoundException("Role with id:" + role.getId() +" do not exist.")
+        );
+        savedUser.addAccessLevel(newRole);
         return new UserDTO(userRepository.save(savedUser));
     }
 
