5f07747595f561b0ae7e819beb500d74b2f26f03
biancaec <biancaec@gmail.com>
2022-11-23 14:14:22 -0300

    Loaded api data to front

--- front/src/app/pages/product/product.component.ts
+++ front/src/app/pages/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import ISku from 'src/app/interfaces/ISku';
 import { SkuService } from 'src/app/service/sku.service';
 
 @Component({
@@ -7,7 +8,7 @@ import { SkuService } from 'src/app/service/sku.service';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
-  skus: any = []
+  skus: ISku[] = []
   constructor(private service:SkuService) {
 
   }
