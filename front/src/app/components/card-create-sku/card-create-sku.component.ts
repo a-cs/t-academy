ee4bea5c0030cef76a09f5fa4f0917f665c6cb44
Luiz Tomasi <netolt123@hotmail.com>
2022-11-24 13:34:11 -0300

    Styling header and footer

---
+++ front/src/app/components/card-create-sku/card-create-sku.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-create-sku',
+  templateUrl: './card-create-sku.component.html',
+  styleUrls: ['./card-create-sku.component.css']
+})
+export class CardCreateSkuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
