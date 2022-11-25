1d9cf1005113963246542d6ce842e1767ddf3abf
Anderson Carneiro Sousa <acarneirosousa@gmail.com>
2022-11-25 11:07:23 -0300

    updating sku card

--- front/src/app/components/card-sku/card-sku.component.ts
+++ front/src/app/components/card-sku/card-sku.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
-import {MatDialog} from '@angular/material/dialog';
+import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import ISku from 'src/app/interfaces/ISku';
 import { ModalUpdateSkuComponent } from 'src/app/components/modal-update-sku/modal-update-sku.component';
@@ -9,42 +9,28 @@ import { ModalUpdateSkuComponent } from 'src/app/components/modal-update-sku/mod
   styleUrls: ['./card-sku.component.css']
 })
 export class CardSkuComponent {
+  @Input() sku: ISku
 
+  constructor(public dialog: MatDialog,
+    private router: Router) { }
 
- @Input() sku: ISku ={
-  name: "",
-  category: {
-    id: 0,
-    name: ""
-  },
-  measurementUnit: {
-    id: 0,
-    description: "",
-    symbol: ""
+  openUpdateDialog(sku: ISku) {
+    console.log(sku)
+    const dialogRef = this.dialog.open(ModalUpdateSkuComponent, {
+      width: "600px",
+      height: "600px",
+      data: sku
+    });
+    // dialogRef.afterClosed().subscribe(
+    //   data => {
+    //     console.log("sku1", sku)
+    //     sku = data
+    //     console.log("sku2", sku)
+    //     this.router.navigate(["/product"])
+    //   }
+    // )
+
   }
-
-}
-
-constructor(public dialog: MatDialog,
-  private router: Router ) {}
-
-openDialog(sku : ISku) {
-  console.log(sku)
-  const dialogRef = this.dialog.open(ModalUpdateSkuComponent,{
-    width: "600px",
-    height: "600px",
-    data:sku
-  });
-  dialogRef.afterClosed().subscribe(
-    data => {
-      console.log("sku1", sku)
-      sku = data
-      console.log("sku2", sku)
-      this.router.navigate(["/product"])
-    }
-)
-
-}
 
 
 }
