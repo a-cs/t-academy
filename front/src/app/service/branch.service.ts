c0d71e3d90fa7d53415f27b7136856f508721f23
biancaec <biancaec@gmail.com>
2022-11-30 08:35:32 -0300

    Created branch service

---
+++ front/src/app/service/branch.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import IBranch from '../interfaces/IBranch';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BranchService {
+
+  constructor(private http: HttpClient) { }
+
+  get(){
+    return this.http.get<IBranch[]>("http://localhost:8080/branch")
+  }
+
+  getById(id:number){
+    return this.http.get<IBranch>(`http://localhost:8080/branch/${id}`)
+  }
+
+  delete(id:number){
+    return this.http.delete(`http://localhost:8080/branch/${id}`)
+  }
+
+  update(id:number, data: IBranch){
+    return this.http.put<IBranch>(`http://localhost:8080/branch/${id}`, data)
+  }
+
+  create(data:IBranch){
+    return this.http.post<IBranch>("http://localhost:8080/branch",data)
+  }
+}
