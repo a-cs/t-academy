f6f782f45997f698e6b603f34f267d47e44dd7f9
HeavyMSV <81862091+heavymsv@users.noreply.github.com>
2022-12-01 08:53:27 -0300

    Finish auth on this service (#42)

--- front/src/app/service/branch.service.ts
+++ front/src/app/service/branch.service.ts
@@ -1,32 +1,53 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import IBranch from '../interfaces/IBranch';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BranchService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private auth:AuthService) { }
 
   get(){
-    return this.http.get<IBranch[]>("http://localhost:8080/branch")
+    return this.http.get<IBranch[]>("http://localhost:8080/branch", {
+
+      headers: this.auth.buildHeader()
+
+    })
   }
 
   getById(id:number){
-    return this.http.get<IBranch>(`http://localhost:8080/branch/${id}`)
+    return this.http.get<IBranch>(`http://localhost:8080/branch/${id}`, {
+
+      headers: this.auth.buildHeader()
+
+    })
   }
 
   delete(id:number){
-    return this.http.delete(`http://localhost:8080/branch/${id}`)
+    return this.http.delete(`http://localhost:8080/branch/${id}`, {
+
+      headers: this.auth.buildHeader()
+
+    })
   }
 
   update(id:number, data: IBranch){
-    return this.http.put<IBranch>(`http://localhost:8080/branch/${id}`, data)
+    return this.http.put<IBranch>(`http://localhost:8080/branch/${id}`, data, {
+
+      headers: this.auth.buildHeader()
+
+    })
   }
 
   create(data:IBranch){
-    return this.http.post<IBranch>("http://localhost:8080/branch",data)
+    return this.http.post<IBranch>("http://localhost:8080/branch",data, {
+
+      headers: this.auth.buildHeader()
+
+    })
   }
 
 }
