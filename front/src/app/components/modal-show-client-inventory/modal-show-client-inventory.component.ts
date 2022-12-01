4571a22d0681b662c060bf035759acfd889cc52e
Luiz Tomasi <netolt123@hotmail.com>
2022-12-01 08:38:50 -0300

    Fixing style of client-inventory-modal

--- front/src/app/components/modal-show-client-inventory/modal-show-client-inventory.component.ts
+++ front/src/app/components/modal-show-client-inventory/modal-show-client-inventory.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import IWarehouseSlot from 'src/app/interfaces/IWarehouseSlot';
+import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-modal-show-client-inventory',
@@ -6,10 +9,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./modal-show-client-inventory.component.css']
 })
 export class ModalShowClientInventoryComponent implements OnInit {
+  form: FormGroup
+  wareHouseSlot: IWarehouseSlot
 
-  constructor() { }
+  constructor(@Inject(MAT_DIALOG_DATA) public data: IWarehouseSlot) { }
 
   ngOnInit(): void {
   }
+
 
 }
