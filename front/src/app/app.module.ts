0a29c3430ae5af8a56baf35521d23e5afa999421
Rafael Prates <rafaelbprates@gmail.com>
2022-12-05 19:34:15 -0400

    Feature/notifications (#54)
    * adds notification to category components
    
    * refactors confirmation dialog
    
    * adds notification to login

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -35,7 +35,6 @@ import { CategoryCreateCardComponent } from './components/CategoryComponents/cat
 import { CategoryComponent } from './pages/category/category.component';
 import { MeasurementUnitComponent } from './pages/measurement-unit/measurement-unit.component';
 
-
 import { CardClientComponent } from './components/ClientComponents/card-client/card-client.component';
 import { ModalUpdateClientComponent } from './components/ClientComponents/modal-update-client/modal-update-client.component';
 import { ClientComponent } from './pages/client/client.component';
@@ -50,6 +49,8 @@ import { BranchComponent } from './pages/branch/branch.component';
 import { ModalShowMoreComponent } from './components/BranchComponents/modal-show-more/modal-show-more.component';
 import { ModalAddBranchComponent } from './components/BranchComponents/modal-add-branch/modal-add-branch.component';
 import { ModalShowClientInventoryComponent } from './components/modal-show-client-inventory/modal-show-client-inventory.component';
+import { ToastrModule } from 'ngx-toastr';
+import { ModalConfirmDeleteComponent } from './components/modal-confirm-delete/modal-confirm-delete.component';
 import { NotFoundComponent } from './components/404/not-found/not-found.component';
 import { NotFoundIconsBgComponent } from './components/404/not-found-icons-bg/not-found-icons-bg.component';
 
@@ -79,7 +80,7 @@ import { NotFoundIconsBgComponent } from './components/404/not-found-icons-bg/no
     CategoryItemCardComponent,
     CategoryUpdateFormComponent,
     CategoryCreateFormComponent,
-    CategoryCreateCardComponent,    
+    CategoryCreateCardComponent,
     CardClientComponent,
     ModalUpdateClientComponent,
     ClientComponent,
@@ -92,6 +93,7 @@ import { NotFoundIconsBgComponent } from './components/404/not-found-icons-bg/no
     ModalAddBranchComponent,
     ModalShowClientInventoryComponent,
     CategoryComponent,
+    ModalConfirmDeleteComponent,
     NotFoundComponent,
     NotFoundIconsBgComponent
   ],
@@ -106,9 +108,10 @@ import { NotFoundIconsBgComponent } from './components/404/not-found-icons-bg/no
     MatAutocompleteModule,
     MatButtonModule,
     FormsModule,
-    MatSelectModule
+    MatSelectModule,
+    ToastrModule.forRoot(),
   ],
-  providers: [],
+  providers: [ModalConfirmDeleteComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- front/src/app/pages/login/login.component.ts
+++ front/src/app/pages/login/login.component.ts
@@ -2,49 +2,63 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { UserService } from 'src/app/service/user.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  form: FormGroup
-  showPassword: boolean = false
+  form: FormGroup;
+  showPassword: boolean = false;
 
-  constructor(private formBuilder: FormBuilder, private userService: UserService, public router: Router) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private userService: UserService,
+    public router: Router,
+    private notificationService: ToastrService
+  ) {}
 
   ngOnInit(): void {
-    this.configureForm()
+    this.configureForm();
   }
 
   configureForm() {
     this.form = this.formBuilder.group({
-      username: ["", [Validators.required]],
-      password: ["", [Validators.required, Validators.minLength(5), Validators.maxLength(20)]],
-    })
+      username: ['', [Validators.required]],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(5),
+          Validators.maxLength(20),
+        ],
+      ],
+    });
   }
 
-  login(){
+  login() {
     // console.log(this.form)
-    let username = this.form.controls['username'].value
-    let password = this.form.controls['password'].value
+    let username = this.form.controls['username'].value;
+    let password = this.form.controls['password'].value;
     // console.log({username, password})
     this.userService.login(username, password).subscribe(
-      response => {
-        console.log("res!", response)
-        localStorage.setItem("T-WMS_token", response.access_token)
-        this.router.navigate([""])
+      (response) => {
+        console.log('res!', response);
+        localStorage.setItem('T-WMS_token', response.access_token);
+        this.router.navigate(['']);
       },
-      error => {
-        console.log("err!", error)
-      })
-
+      (error) => {
+        this.notificationService.error('Login or password invalid', 'Error!', {
+          progressBar: true,
+        });
+      }
+    );
   }
 
-
   togglePasswordVisibility(item: any) {
-    this.showPassword = !this.showPassword
-    item.focus()
+    this.showPassword = !this.showPassword;
+    item.focus();
   }
 }
