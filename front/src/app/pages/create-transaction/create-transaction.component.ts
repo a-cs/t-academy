543a18ced5bef79bcf337816f5351e12bdc1afbf
Anderson Carneiro Sousa <acarneirosousa@gmail.com>
2022-12-08 08:39:46 -0300

    updating toasts

--- front/src/app/pages/create-transaction/create-transaction.component.ts
+++ front/src/app/pages/create-transaction/create-transaction.component.ts
@@ -13,176 +13,184 @@ import { TransactionService } from 'src/app/service/transaction.service';
 import ISku from '../../interfaces/ISku';
 
 @Component({
-  selector: 'app-create-transaction',
-  templateUrl: './create-transaction.component.html',
-  styleUrls: ['./create-transaction.component.css']
+    selector: 'app-create-transaction',
+    templateUrl: './create-transaction.component.html',
+    styleUrls: ['./create-transaction.component.css']
 })
 export class CreateTransactionComponent implements OnInit {
-  skus: ISku[]
-  clients: IClient[]
-  filteredSkus: Observable<ISku[]>
-  filteredClients: Observable<IClient[]>
-  firstSku?: ISku
+    skus: ISku[]
+    clients: IClient[]
+    filteredSkus: Observable<ISku[]>
+    filteredClients: Observable<IClient[]>
+    firstSku?: ISku
 
-  form: FormGroup
+    form: FormGroup
 
-  constructor(
-    private skuService: SkuService,
-    private clientService: ClientService,
-    private transactionService: TransactionService,
-    private auth: AuthService,
-    private notification: ToastrService,
-    private formBuilder: FormBuilder) { }
+    constructor(
+        private skuService: SkuService,
+        private clientService: ClientService,
+        private transactionService: TransactionService,
+        private auth: AuthService,
+        private notification: ToastrService,
+        private formBuilder: FormBuilder) { }
 
-  ngOnInit(): void {
+    ngOnInit(): void {
 
-    this.skuService.get().subscribe(
-      data => {
-        this.onGetSkus(data)
-      }
-    )
-    this.clientService.get().subscribe(
-      data => {
-        this.onGetClients(data)
-      }
-    )
-
-    this.configureForm()
-
-
-    // this.filteredSkus.subscribe(ele => this.firstSku = ele[0])
-
-  }
-
-  onGetSkus(data: ISku[]) {
-    this.skus = data
-    this.filteredSkus = this.form.controls["sku"].valueChanges.pipe(
-      startWith(''),
-      map((value: any) => {
-        const name = value = typeof value === 'string' ? value : value?.name;
-        return name ? this._filterSkus(name as string) : this.skus.slice();
-      }),
-    );
-  }
-
-  private _filterSkus(name: string): ISku[] {
-    const filterValue = name.toLowerCase();
-    return this.skus.filter(sku => sku.name?.toLowerCase().includes(filterValue));
-  }
-
-  onGetClients(data: IClient[]) {
-    this.clients = data
-    this.filteredClients = this.form.controls["client"].valueChanges.pipe(
-      startWith(''),
-      map((value: any) => {
-        const name = value = typeof value === 'string' ? value : value?.name;
-        return name ? this._filterClients(name as string) : this.clients.slice();
-      }),
-    );
-  }
-
-  private _filterClients(name: string): IClient[] {
-    const filterValue = name.toLowerCase();
-    return this.clients.filter(client => client.name?.toLowerCase().includes(filterValue));
-  }
-
-  configureForm() {
-    this.form = this.formBuilder.group({
-      sku: [null, [Validators.required]],
-      client: [null, [Validators.required]],
-      quantity: [0, [Validators.required]],
-      type: ["", [Validators.required]]
-    })
-  }
-
-
-
-
-  skuFallback() { }
-
-  clientFallback() { }
-
-  displaySku(sku: ISku): string {
-    return sku && sku.name ? sku.name : '';
-  }
-
-  displayClient(client: IClient): string {
-    return client && client.name ? client.name : '';
-  }
-
-  createTransaction() {
-    const branchId = this.auth.getUserBranchId()
-    if (branchId != null) {
-      const newTransaction: ITransactionPayload = {
-        user: {
-          id: this.auth.getUserId() as unknown as number
-        },
-        type: this.form.controls["type"].value,
-        client:{
-          id:this.form.controls["client"].value.id,
-        },
-        sku: {
-          id: this.form.controls["sku"].value.id,
-        },
-        quantity: this.form.controls["quantity"].value,
-        warehouseSlot: {
-          warehouseSlotId: {
-            branch: {
-              id: this.auth.getUserBranchId()
+        this.skuService.get().subscribe(
+            data => {
+                this.onGetSkus(data)
             }
-          }
-        }
-        // user: {
-        //   id: this.auth.getUserId() as unknown as number
-        // },
-        // type: this.form.controls["type"].value,
-        // client: this.form.controls["client"].value,
-        // sku: this.form.controls["sku"].value,
-        // quantity: this.form.controls["quantity"].value,
-        // warehouseSlot: {
-        //   warehouseSlotId: {
-        //     branch: {
-        //       id: this.auth.getUserBranchId()
-        //     }
-        //   }
-        // }
-      }
-      console.log(newTransaction)
-      this.transactionService.create(newTransaction).subscribe(
-        (respose : ITransaction[]) => {
-          respose.map(transaction => {
-            console.log(transaction)
-            console.log(transaction.type)
-            let msg = ""
-            if(transaction.type === "IN")
-              msg = `Transaction of type ${transaction.type.toLowerCase()} created successfully!`
-            if(transaction.type === "OUT")
-              msg = `Transaction of type ${transaction.type.toLowerCase()} created successfully! </br> Remove item from ----`
-            console.log(msg)
-            this.notification.success(
-              msg,
-              'Created!',
-              {
-                progressBar: true,
-                enableHtml: true
-              }
-            );
-          })
-          console.log(respose)
-         },
-        (error) => {
-          console.log(error)
-          this.notification.error(error.error.message, 'Error', {
-            progressBar: true,
-          });
-        }
-      );
-    }
-    else {
-      this.notification.error("Only users with a Branch assigned, are allowed to create transactions!", 'Error', {
-        progressBar: true,
-      });
+        )
+        this.clientService.get().subscribe(
+            data => {
+                this.onGetClients(data)
+            }
+        )
+
+        this.configureForm()
+
+
+        // this.filteredSkus.subscribe(ele => this.firstSku = ele[0])
+
     }
 
-  }
+    onGetSkus(data: ISku[]) {
+        this.skus = data
+        this.filteredSkus = this.form.controls["sku"].valueChanges.pipe(
+            startWith(''),
+            map((value: any) => {
+                const name = value = typeof value === 'string' ? value : value?.name;
+                return name ? this._filterSkus(name as string) : this.skus.slice();
+            }),
+        );
+    }
+
+    private _filterSkus(name: string): ISku[] {
+        const filterValue = name.toLowerCase();
+        return this.skus.filter(sku => sku.name?.toLowerCase().includes(filterValue));
+    }
+
+    onGetClients(data: IClient[]) {
+        this.clients = data
+        this.filteredClients = this.form.controls["client"].valueChanges.pipe(
+            startWith(''),
+            map((value: any) => {
+                const name = value = typeof value === 'string' ? value : value?.name;
+                return name ? this._filterClients(name as string) : this.clients.slice();
+            }),
+        );
+    }
+
+    private _filterClients(name: string): IClient[] {
+        const filterValue = name.toLowerCase();
+        return this.clients.filter(client => client.name?.toLowerCase().includes(filterValue));
+    }
+
+    configureForm() {
+        this.form = this.formBuilder.group({
+            sku: [null, [Validators.required]],
+            client: [null, [Validators.required]],
+            quantity: [0, [Validators.required]],
+            type: ["", [Validators.required]]
+        })
+    }
+
+
+
+
+    skuFallback() { }
+
+    clientFallback() { }
+
+    displaySku(sku: ISku): string {
+        return sku && sku.name ? sku.name : '';
+    }
+
+    displayClient(client: IClient): string {
+        return client && client.name ? client.name : '';
+    }
+
+    createTransaction() {
+        const branchId = this.auth.getUserBranchId()
+        if (branchId != null) {
+            const newTransaction: ITransactionPayload = {
+                user: {
+                    id: this.auth.getUserId() as unknown as number
+                },
+                type: this.form.controls["type"].value,
+                client: {
+                    id: this.form.controls["client"].value.id,
+                },
+                sku: {
+                    id: this.form.controls["sku"].value.id,
+                },
+                quantity: this.form.controls["quantity"].value,
+                warehouseSlot: {
+                    warehouseSlotId: {
+                        branch: {
+                            id: this.auth.getUserBranchId()
+                        }
+                    }
+                }
+                // user: {
+                //   id: this.auth.getUserId() as unknown as number
+                // },
+                // type: this.form.controls["type"].value,
+                // client: this.form.controls["client"].value,
+                // sku: this.form.controls["sku"].value,
+                // quantity: this.form.controls["quantity"].value,
+                // warehouseSlot: {
+                //   warehouseSlotId: {
+                //     branch: {
+                //       id: this.auth.getUserBranchId()
+                //     }
+                //   }
+                // }
+            }
+            console.log(newTransaction)
+            this.transactionService.create(newTransaction).subscribe(
+                (respose: ITransaction[]) => {
+                    respose.map(transaction => {
+                        console.log(transaction)
+                        console.log(transaction.type)
+                        let msg = ""
+                        let title = ""
+                        if (transaction.type === "IN") {
+                            msg = `Transaction of type ${transaction.type.toLowerCase()} created successfully!`
+                            title = "In!"
+                        }
+                        if (transaction.type === "OUT") {
+                            msg = `Transaction of type ${transaction.type.toLowerCase()} created successfully! </br> Remove item from ----`
+                            title = "Out!"
+                        }
+                        console.log(msg)
+                        this.notification.success(
+                            msg,
+                            title,
+                            {
+                                tapToDismiss: true,
+                                disableTimeOut: true,
+                                closeButton: true,
+                                // progressBar: true,
+                                enableHtml: true
+                            }
+                        );
+                        console.log(respose)
+                    })
+                },
+                (error) => {
+                    console.log(error)
+                    this.notification.error(error.error.message, 'Error', {
+                        progressBar: true,
+                    });
+                }
+            );
+        }
+        else {
+            this.notification.error("Only users with a Branch assigned, are allowed to create transactions!", 'Error', {
+                progressBar: true,
+            });
+        }
+
+    }
 }
