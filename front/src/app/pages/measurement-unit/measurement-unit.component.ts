f19cb49be298bcac07c45965093b6045a566a9ad
Anderson Carneiro Sousa <63657280+a-cs@users.noreply.github.com>
2022-12-12 13:57:52 -0300

    Feature/adding spinner (#96)
    * adding spinners during load
    
    * adding more spinners

--- front/src/app/pages/measurement-unit/measurement-unit.component.ts
+++ front/src/app/pages/measurement-unit/measurement-unit.component.ts
@@ -4,45 +4,71 @@ import IMeasurementUnit from 'src/app/interfaces/IMeasurementUnit';
 import { AuthService } from 'src/app/service/auth.service';
 import { MeasurementUnitService } from 'src/app/service/measurement-unit.service';
 import { buttonPermission } from 'src/app/utils/utils';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
-  selector: 'app-measurement-unit',
-  templateUrl: './measurement-unit.component.html',
-  styleUrls: ['./measurement-unit.component.css'],
+    selector: 'app-measurement-unit',
+    templateUrl: './measurement-unit.component.html',
+    styleUrls: ['./measurement-unit.component.css'],
 })
 export class MeasurementUnitComponent implements OnInit {
-  component = ModalAddMeasuUnitComponent;
-  measurementUnits: IMeasurementUnit[];
-  searchText: string;
+    component = ModalAddMeasuUnitComponent;
+    measurementUnits: IMeasurementUnit[];
+    searchText: string;
+    isLoading: boolean = false;
+    isError: boolean = false;
 
-  constructor(
-    private measurementUnitService: MeasurementUnitService,
-    public auth: AuthService
-  ) {}
+    constructor(
+        private measurementUnitService: MeasurementUnitService,
+        public auth: AuthService,
+        private notification: ToastrService
+    ) { }
 
-  btnPermission = buttonPermission;
+    btnPermission = buttonPermission;
 
-  ngOnInit(): void {
-    this.measurementUnitService.measurementUnitChanged.subscribe(() =>
-      this.getData()
-    );
-    this.getData();
-  }
+    ngOnInit(): void {
+        this.measurementUnitService.measurementUnitChanged.subscribe(() =>
+            this.getData()
+        );
+        this.getData();
+    }
 
-  getData() {
-    this.measurementUnitService.get().subscribe((data) => {
-      this.measurementUnits = data;
-      console.log(this.measurementUnits);
-    });
-  }
+    getData() {
+        this.isError = false
+        this.isLoading = true
+        this.measurementUnitService.get().subscribe((data) => {
+            this.measurementUnits = data;
+            console.log(this.measurementUnits);
+            this.isLoading = false
+        }, error => {
+            this.isLoading = false
+            this.isError = true
+            this.notification.error(error.error.message, 'Error: No serve response', {
+              tapToDismiss: true,
+              disableTimeOut: true,
+              closeButton: true,
+            });
+          });
+    }
 
-  onSearchTextEntered(searchValue: string) {
-    this.searchText = searchValue;
-    this.measurementUnitService
-      .getByLikeName(this.searchText)
-      .subscribe((data) => {
-        this.measurementUnits = data;
-        console.log(this.measurementUnits);
-      });
-  }
+    onSearchTextEntered(searchValue: string) {
+        this.isError = false
+        this.isLoading = true
+        this.searchText = searchValue;
+        this.measurementUnitService
+            .getByLikeName(this.searchText)
+            .subscribe((data) => {
+                this.measurementUnits = data;
+                console.log(this.measurementUnits);
+                this.isLoading = false
+            }, error => {
+                this.isLoading = false
+                this.isError = true
+                this.notification.error(error.error.message, 'Error: No serve response', {
+                  tapToDismiss: true,
+                  disableTimeOut: true,
+                  closeButton: true,
+                });
+              });
+    }
 }
